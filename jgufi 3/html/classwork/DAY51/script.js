9068a577ad6d5dd5f9c1927eaefe5235bab50102
materekh <87072420+materekh@users.noreply.github.com>
2024-01-14 13:45:00 +0400

    commit

---
+++ jgufi 3/html/classwork/DAY51/script.js
@@ -0,0 +1,12 @@
+const student= {
+    firstName:"mate",
+    lastName:"rekhviashvili",
+    age:13,
+
+};
+const teacher= {
+    firstName:"luka",
+    lastName:"tskhvaradze",
+    age:17,
+};
+console.log(student.firstName);
--- jgufi 3/html/homework/DAY50/Banking System/script.js
+++ jgufi 3/html/homework/DAY50/Banking System/script.js
@@ -3,6 +3,10 @@ var mon=document.getElementById("money");
 var depoButton=document.getElementById("depo");
 var widthButton=document.getElementById("widt");
 var money=0;
+function frame(){
+    money=money*1.00124456518;
+    mon.textContent=Math.round(money*100)/100;
+}
 depoButton.addEventListener('click',function(){
     money+=Number(form.value);
     mon.textContent=money;
@@ -16,3 +20,4 @@ widthButton.addEventListener('click',function(){
         mon.textContent=money;
     }
 })
+setInterval(frame,125);
