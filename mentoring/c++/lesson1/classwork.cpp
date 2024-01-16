856f2231cca4bf0bf4a6ce3061d72b1b65de2ed4
materekh <87072420+materekh@users.noreply.github.com>
2024-01-16 20:48:26 +0400

    commit

---
+++ mentoring/c++/lesson1/classwork.cpp
@@ -0,0 +1,30 @@
+// abergsdfg
+
+
+// fgsdfgsdf
+// e
+// registerser
+// gs
+// decltype
+
+
+
+
+// makrateli
+// sdfg
+// __STDCPP_DEFAULT_NEW_ALIGNMENT__gs
+
+
+
+
+// fgsdfgsdf
+
+
+
+
+// makrateli
+
+
+
+
+// eqwerqwerqwerrwqrw
