e7326858c7a9d238108fdd7799dd040f7ec4157c
materekh <87072420+materekh@users.noreply.github.com>
2024-02-12 22:28:26 +0400

    commit

---
+++ jgufi 3/html/classwork/DAY59/script.js
@@ -0,0 +1,16 @@
+//1
+let counter=0
+while(true){
+    console.log(counter)
+    if(counter++===20) break
+}
+
+//2
+counter=1
+while(counter<=100) console.log(counter++)
+
+//3
+let age=10
+if(age<15) console.log(1)
+else if(age===15) console.log(2)
+else console.log(3)
