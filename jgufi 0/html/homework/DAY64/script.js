025043d8adba459676ebf79426089650930956d4
materekh <87072420+materekh@users.noreply.github.com>
2024-03-11 09:58:28 +0400

    commit

---
+++ jgufi 0/html/homework/DAY64/script.js
@@ -0,0 +1,23 @@
+// let secs = Number(prompt("enter seconds: "));
+// while(secs > 0){
+//     document.write(`${secs} secs left `)
+//     secs--
+// }
+
+// let i=0,arr=[];
+// while(i<50){
+//     i++;
+//     if(i==1) continue;
+//     let prime=true;
+//     for(let j=2;j<i;j++) if(i%j==0) prime=false;
+//     if(prime) arr.push(i);
+// }
+// document.write(arr)
+
+// let person={
+//     name:"Mate",
+//     age:13,
+//     occupation:"mini leader"
+// }
+// console.log(person.name,person.age,person.occupation)
+
