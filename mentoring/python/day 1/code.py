cfa93754dc3176832d97473c780ac110a348767e
materekh <87072420+materekh@users.noreply.github.com>
2024-03-18 20:09:34 +0400

    fix

---
+++ mentoring/python/day 1/code.py
@@ -0,0 +1,28 @@
+# # integer - int
+# a = 1
+
+# # string - str
+# b = "luka"
+
+# # float
+# c = 5.2 
+
+# print(type(c))
+
+# a = "3"
+
+# b = int(a)
+# c = float(a)
+# a -> "3"
+# b -> 3
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(c)
+
+# print(float(int(3.14)))
+
+# luka = "23"
+
+# print("luka")
+# print(luka)
